a93349bbff113444dead104b0e26a5aa3f534c6b
Huy Sad Nhan <cv.nhuytyl2603@gmail.com>
2023-12-12 19:13:19 +0700

    update 12/12

--- src/app/trangchu/trangchu.component.ts
+++ src/app/trangchu/trangchu.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-trangchu',
   templateUrl: './trangchu.component.html',
   styleUrls: ['./trangchu.component.css']
 })
-export class TrangchuComponent {
 
+export class TrangchuComponent implements OnInit {
+  Trangchu: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 1
+      }
+    },
+    nav: true
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
